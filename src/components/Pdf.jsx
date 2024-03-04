675050345ec7ac96315dbb9567b78181491f15da
AlbertLnz <albert2000_lanza@hotmail.com>
2024-03-04 09:37:10 +0100

    Section in PDF (View Component)

--- src/components/Pdf.jsx
+++ src/components/Pdf.jsx
@@ -1,10 +1,15 @@
-import { Document, Text, Page, Image, StyleSheet } from "@react-pdf/renderer";
+import { Document, Text, Page, Image, StyleSheet, View } from "@react-pdf/renderer";
 import image from '../assets/image.png'
 
 const styles = StyleSheet.create({
   page: {
     backgroundColor: '#202020',
     color: 'white'
+  },
+  section: {
+    display: 'flex',
+    flexDirection: 'row',
+    margin: 10,
   }
 })
 
@@ -13,8 +18,10 @@ const Pdf = () => {
     <Document>
       <Page style={styles.page}>
         <Text>Title 1</Text>
-        <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti officiis non nesciunt vero soluta iste iure, ad saepe maiores labore at eligendi repellendus reprehenderit dicta asperiores vitae, nostrum sint mollitia sit culpa? Quia eius, error eos sequi aspernatur ipsum, blanditiis dolore quibusdam aut in cumque ducimus laborum perferendis tempora architecto.</Text>
-        <Image src={image} />
+        <View style={styles.section}>
+          <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti officiis non nesciunt vero soluta iste iure, ad saepe maiores labore at eligendi repellendus reprehenderit dicta asperiores vitae, nostrum sint mollitia sit culpa? Quia eius, error eos sequi aspernatur ipsum, blanditiis dolore quibusdam aut in cumque ducimus laborum perferendis tempora architecto.</Text>
+          <Image src={image} />
+        </View>
       </Page>
     </Document>
   )
